5cf47629c2edcb991c3a597396518663e2dae313
montohang <harmonsitohang17@gmail.com>
2021-03-06 21:00:09 +0700

    [Add main screen]

---
+++ lib/ui/screens/main_screen.dart
@@ -0,0 +1,15 @@
+part of 'screens.dart';
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('woii'),
+    );
+  }
+}
